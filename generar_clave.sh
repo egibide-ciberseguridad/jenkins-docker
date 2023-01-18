a5d796116837b0b36c9d343962e723c6c56a522c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-01-18 09:12:10 +0100

    Mostrar información de depuración en el script

--- generar_clave.sh
+++ generar_clave.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-mkdir -p ~/.ssh && chmod 0700 ~/.ssh && ssh-keyscan "$1" >>~/.ssh/known_hosts 2>/dev/null
+mkdir -p ~/.ssh && chmod 0700 ~/.ssh
 
 cd ~/.ssh || exit
+
+ssh-keyscan "$1" >>~/.ssh/known_hosts 2>/dev/null
+
+echo
+if [ -s ~/.ssh/known_hosts ]; then
+  echo "Fichero de claves conocidas actualizado para el host: $1"
+else
+  echo "ERROR: No se ha podido actualizar el fichero de claves conocidas del host: $1"
+fi
 
 rm -f id_rsa
 rm -f id_rsa.pub
